7820f55a4d73a7c7f26f078548e059967304dcbc
Momin Mahdihusain <cs5140288@malkauns.cse.iitd.ernet.in>
2017-09-27 15:16:17 +0530

    gaussian blur

--- wls_filter.py
+++ wls_filter.py
@@ -8,6 +8,9 @@ from scipy.sparse.linalg import spsolve
 
 def wlsfilter_layer(image_orig, cmat, beta=0.2 ,lambda_=0.2):
 	
+
+	gmat = cv2.GaussianBlur(cmat,(5,5),0)
+
 	image = image_orig.astype(np.float)/255.0
 	image1 = image.flatten(1)
 	s = image.shape
@@ -19,10 +22,10 @@ def wlsfilter_layer(image_orig, cmat, beta=0.2 ,lambda_=0.2):
 	dy = -beta*lambda_ / ((np.absolute(dy) ** 1.2 + 0.0001))
 	dx = -beta*lambda_ / ((np.absolute(dx) ** 1.2 + 0.0001))
 	
-	for y in xrange(s[0]):
-		for x in xrange(s[1]):
-			dy[y][x] = (cmat[y][x]==1)*dy[y][x]
-			dx[y][x] = (cmat[y][x]==1)*dx[y][x]
+	for y in xrange(s[0]-1):
+		for x in xrange(s[1]-1):
+			dy[y][x] = (gmat[y][x]==1)*dy[y][x]
+			dx[y][x] = (gmat[y][x]==1)*dx[y][x]
 
 	dy = np.vstack((dy, np.zeros(s[1], )))
 	dx = np.hstack((dx, np.zeros(s[0], )[:, np.newaxis]))
@@ -41,3 +44,29 @@ def wlsfilter_layer(image_orig, cmat, beta=0.2 ,lambda_=0.2):
 	detail = image - base
 	
 	return (base*255.0), (detail*255.0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
