9477801a33fb2c0c08fb4ed4578752d2da6635a7
Momin Mahdihusain <cs5140288@malkauns.cse.iitd.ernet.in>
2017-09-27 15:19:51 +0530

    gaussian blur

--- wls_filter.py
+++ wls_filter.py
@@ -7,14 +7,21 @@ from scipy.sparse import spdiags
 from scipy.sparse.linalg import spsolve
 
 def wlsfilter_layer(image_orig, cmat, beta=0.2 ,lambda_=0.2):
-	
-
-	gmat = cv2.GaussianBlur(cmat,(5,5),0)
 
 	image = image_orig.astype(np.float)/255.0
 	image1 = image.flatten(1)
 	s = image.shape
 	k = np.prod(s)
+	
+	gmat = cmat.copy()
+	for y in xrange(s[0]):
+		for x in xrange(s[1]):
+			if not gmat[y][x]==1:
+				gmat[y][x]=0
+
+	gmat = cv2.GaussianBlur(gmat,(5,5),0)
+
+
 
 	dy = np.diff(image, 1, 0)
 	dx = np.diff(image, 1, 1)
@@ -24,8 +31,8 @@ def wlsfilter_layer(image_orig, cmat, beta=0.2 ,lambda_=0.2):
 	
 	for y in xrange(s[0]-1):
 		for x in xrange(s[1]-1):
-			dy[y][x] = (gmat[y][x]==1)*dy[y][x]
-			dx[y][x] = (gmat[y][x]==1)*dx[y][x]
+			dy[y][x] = (gmat[y][x])*dy[y][x]
+			dx[y][x] = (gmat[y][x])*dx[y][x]
 
 	dy = np.vstack((dy, np.zeros(s[1], )))
 	dx = np.hstack((dx, np.zeros(s[0], )[:, np.newaxis]))
